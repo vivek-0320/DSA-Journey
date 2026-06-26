29f620733cedcf064ecd2df45ecf449cdaf342be
Vivek Kesarwani <134508204+CricketFan18@users.noreply.github.com>
2026-06-26 21:06:49 +0530

    refactored files into categories of topic

--- Queue/SingletonObject.java
+++
@@ -1,9 +0,0 @@
-public class SingletonObject {
-    private static SingletonObject ob;
-
-    public static SingletonObject getInstance() {
-        if (ob == null)
-            ob = new SingletonObject();
-        return ob;
-    }
-}
